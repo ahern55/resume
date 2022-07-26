c350d6abdfdada2bb422ca49cb2778c82b15e60f
Jason Ahern <jason.ahern53@gmail.com>
2022-07-25 21:11:22 -0700

    update publish script to publish directly to website dir

--- publish.sh
+++ publish.sh
@@ -1,5 +1,15 @@
-cp build/resume/resume.pdf ./resume.pdf
+RESUME_DIR=$(pwd)
+WEBSITE_DIR=~/projects/ahern55.github.io/resources/
 
-git add resume.pdf
-git commit -m "publish resume $(date '+%d-%m-%Y')  ✏️"
-git push
+publishToCurrentDir() {
+  git add resume.pdf
+  git commit -m "publish resume $(date '+%d-%m-%Y')  ✏️"
+  git push
+}
+
+cp build/resume/resume.pdf ./resume.pdf
+publishToCurrentDir
+
+cp build/resume/resume.pdf $WEBSITE_DIR/resume.pdf
+cd $WEBSITE_DIR
+publishToCurrentDir
